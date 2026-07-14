5742e3d5e2a61989f46040fd99fada0af01db23d
yan-930521 <yan930521@gmail.com>
2026-07-14 13:42:43 +0800

    feat(core): establish index.ts export boundary for core module

---
+++ src/core/index.ts
@@ -0,0 +1,6 @@
+// src/core/index.ts
+// Export all necessary infrastructure modules for the package to use.
+// Note: Additional exports (like DataBlock, IBus) should be added here as they are developed
+export * from './config/Config';
+export * from './messaging/DataBlock';
+export * from './messaging/IBus';
